a5d24a557508acf1a812390490944a7be7afe974
jul0037 <119369348+jul0037@users.noreply.github.com>
2023-11-14 21:57:53 +0100

    Delete delete.php

--- delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-?>
-<a>Are you sure you want to delete that Objekt?</a>
-<form method="POST" action="index.html <?php unlink($_GET["name"]);?>">
-    <input type="submit" name="button_0" value="delete">
-</form>
